8603ffd97af2ce71eb2265091a87df552cbe8d28
Payam Rafiee <payam.rafiee@gmail.com>
2023-11-17 10:50:23 +0330

    Update finetune_task.sh

--- scripts/v1_5/finetune_task.sh
+++ scripts/v1_5/finetune_task.sh
@@ -4,8 +4,8 @@ deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path liuhaotian/llava-v1.5-7b \
     --version v1 \
-    --data_path ./playground/data/llava_v1_5_mix665k.json \
-    --image_folder ./playground/data \
+    --data_path ./playground/data/output.json \
+    --image_folder ./playground/data/images \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
@@ -14,7 +14,7 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-task \
+    --output_dir ./checkpoints/llava-v1.5-7b-task \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
